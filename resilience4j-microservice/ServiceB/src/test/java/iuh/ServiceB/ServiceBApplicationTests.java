5ba86078f9a74d36acffb51ff1351f084cf5a361
Lê Hồng Ngân <94158792+lehongngan98@users.noreply.github.com>
2024-04-03 16:17:52 +0700

    Add files via upload

---
+++ resilience4j-microservice/ServiceB/src/test/java/iuh/ServiceB/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.ServiceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
